09efb41fa652c54e6a2c7910b2a8e5e390663ec4
Chongdan Pan <panda@panchongdan@foxmail.com>
2022-04-26 13:50:02 -0400

    modify build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 NAME=MsgRobot
-CONDA_PATH=/home/panda
 set -e
-rm -rf $CONDA_PATH/anaconda3/lib/python3.8/site-packages/$NAME
-cp -rf $NAME $CONDA_PATH/anaconda3/lib/python3.8/site-packages/
+rm -rf $CONDA_PREFIX/lib/python3.9/site-packages/$NAME
+cp -rf $NAME $CONDA_PREFIX/lib/python3.9/site-packages/
